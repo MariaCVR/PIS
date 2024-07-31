51f1ed48ff384b880a5a9158389b0a0ea183c5f4
AlexInfante <danielinfante844@gmail.com>
2024-07-30 19:54:58 -0500

    Update seguidor_solar.ino

--- seguidor_solar.ino
+++ seguidor_solar.ino
@@ -1,109 +1,186 @@
+#include <RtcDS1302.h>
 #include <Servo.h>
 
-//Definiendo Servos
-Servo servohori; //Servo horizontal
-int servoh = 100; 
-int servohLimitHigh = 160; //Valor maximo de movimiento de 160
-int servohLimitLow = 20; //Valor minimod de movimiento de 20
+ThreeWire myWire(4,5,2); // IO, SCLK, CE
+RtcDS1302<ThreeWire> Rtc(myWire);
 
-Servo servoverti; //Servo vertical
-int servov = 100; 
-int servovLimitHigh = 160; //Valor maximo de movimiento de 160
-int servovLimitLow = 20; //Valor minimod de movimiento de 20
+Servo servohori; // servo horizontal y limites
+int servoh=90;
+int servohLimitHigh = 180;
+int servohLimitLow = 35;
 
-//Asignacion de variables LDR
-int ldrsup_i = 2; //LDR superior izquierdo
-int ldrsup_d = 1; //LDR superior derecho
-int ldrinf_i = 3; //LDR inferior izquierdo
-int ldrinf_d = 0; //LDR inferior derecho
+Servo servoverti; // servo vertical y limites
+int servov=90;
+int servovLimitHigh = 180;
+int servovLimitLow = 35;
+unsigned long time;
+float tiempo = 200000.50; // CADA 200 SEGUNDOS AVANZA UN GRADO, HASTA LLEGAR A LOS 180°
+unsigned long t = 0;
 
-void setup () //Parte que se ejecutara una sola vez
+int pos_default = 90;
+int pos_default_add = 90;
+boolean act_1 = false, act_2 = false;
+char DiaSemana[][4] = {"Dom", "Lun", "Mar", "Mie", "Jue", "Vie", "Sab"};
+
+
+// Pines LDR
+int ldrtopr = 3; // pin LDR A3 superior derecho
+int ldrtopl = 0; // pin LDR A0 superior izquierdo
+int ldrbotr = 2; // pin LDR A2 inferior derecho
+int ldrbotl = 1; // pin LDR A1 inferior izquierdo
+
+void setup() 
 {
-  servohori.attach(10);
-  servohori.write(100); //Ubica al servo horizontal en la posicion 0
-  servoverti.attach(9);
-  servoverti.write(100); //Ubica al servo vertical en la posicion 0
-  delay(500); //Retraso o delay de 500 milisegundos
+    Serial.begin(9600);
+    Rtc.Begin();
+
+    RtcDateTime compiled = RtcDateTime(_DATE, __TIME_);
+    if (!Rtc.IsDateTimeValid()) 
+    {
+        Serial.println("RTC lost confidence in the DateTime!");
+        Rtc.SetDateTime(compiled);
+    }
+
+    if (Rtc.GetIsWriteProtected())
+    {
+        Serial.println("RTC was write protected, enabling writing now");
+        Rtc.SetIsWriteProtected(false);
+    }
+
+    if (!Rtc.GetIsRunning())
+    {
+        Serial.println("RTC was not actively running, starting now");
+        Rtc.SetIsRunning(true);
+    }
+
+    RtcDateTime now = Rtc.GetDateTime();
+    if (now < compiled) 
+    {
+        Serial.println("RTC is older than compile time! Updating");
+        Rtc.SetDateTime(compiled);
+    }
+  
+    servohori.attach(10);
+    servohori.write(90);
+    servoverti.attach(9);
+    servoverti.write(90);
+    delay(5000);
 }
 
-void loop() //Parte que se repetira constantemente
+void loop() 
 {
-  servoh = servohori.read(); //Lee los valores actuales del servo horizontal
-  servov = servoverti.read(); //Lee los valore actuales del servo vertical
-  //Asignando valores analogicos de cada LDR
-  int sup_i = analogRead(ldrsup_i); //Valor del LDR superior izquierdo
-  int sup_d = analogRead(ldrsup_d); //Valor del LDR superior derecho
-  int inf_i = analogRead(ldrinf_i); //Valor del LDR inferior izquierdo
-  int inf_d = analogRead(ldrinf_d); //Valor del LDR inferior derecho
+    int LDR = analogRead(A1);
+    Serial.print("SENSOR_LDR ");
+    Serial.println(LDR);
+ 
+    RtcDateTime now = Rtc.GetDateTime();
+    printDateTime(now);
+    delay(1000);
 
-  // calculando el Promedio de los LDRs
-
-  int promsup = (sup_i + sup_d) / 2; //Promedio de los LDRs superiores
-  int prominf = (inf_i + inf_d) / 2; //Promedio de los LDRs inferiores
-  int prom_i = (sup_i + inf_i) / 2; //Promedio de los LDRs izquierdos
-  int prom_d = (sup_d + inf_d) / 2; //Promedio de los LDRs derechos
-
-  //Condicion para el movimiento de los servos
-
-  /*Si el promedio de los LDRs superiores es menor al de los LDRs inferiores
-  el servo vertical se movera hacia arriba*/
-  if (promsup < prominf)
-  {
-    servoverti.write(servov +1);
-
-    //Condicion que evita que el servo se pase del valor limite
-    if (servov > servovLimitHigh) 
-     { 
-      servov = servovLimitHigh;
-     }
-    delay(10); //Retraso o delay de 10 milisegundos
-  }
-  /*Si el promedio de los LDRs superiores es mayor al de los LDRs inferiores
-  el servo vertical se movera hacia abajo*/
-  else if (prominf < promsup)
-  {
-    servoverti.write(servov -1);
-    
-    //Condicion que evita que el servo se pase del valor limite
-    if (servov < servovLimitLow)
-    {
-      servov = servovLimitLow;
+    if (LDR <= 400) {
+        servoverti.write(pos_default);
+        servohori.write(pos_default);
+        act_1 = true; 
     }
-    delay(10); //Retraso o delay de 10 milisegundos
-  }
-  //De otro modo, se mantendra en el mismo valor
-  else 
-  {
-    servoverti.write(servov);
-  }
-  /*Si el promedio de los LDRs izquierdos es mayor al de los LDRs derechos
-  el servo horizontal se movera hacia la derecha*/
-  if (prom_i > prom_d)
-  {
-    servohori.write(servoh +1);
-    //Condicion que evita que el servo pase del valor limite
-    if (servoh > servohLimitHigh)
-    {
-    servoh = servohLimitHigh;
+    if (now.Hour() >= 7 && now.Hour() < 17) {
+        act_1 = true;
     }
-    delay(10); //Retraso o delay de 10 milisegundos
-  }
-  /*Si el promedio de los LDRs derechos es mayor al de los LDRs izquierdos
-  el servo horizontal se movera hacia la izquierda*/
-  else if (prom_d > prom_i)
-  {
-    servohori.write(servoh -1);
-    //Condicion que evita que el servo pase del valor limite
-    if (servoh < servohLimitLow)
-     {
-     servoh = servohLimitLow;
-     }
-    delay(10); //Retraso o delay de 10 milisegundos
-  }
-  //De otro modo, se mantendra en el mismo valor
-  else 
-  {
-    servohori.write(servoh);
-  }
-  delay(50); //Retraso o delay de 50 milisegundos
+
+    if (act_1 == true) {
+        time = millis();
+        if (time - t > tiempo) {
+            t = time;
+            pos_default_add++;
+        }
+        if (LDR < 600) {
+            servoverti.write(pos_default_add);
+            Serial.print("GRADOS SERVO");
+            Serial.println(pos_default_add);
+        } else {
+            act_2 = true; // Activar el seguimiento con fotorresistencias cuando la luz es alta
+        }
+    }
+
+    if (pos_default_add >= 180) {
+        act_1 = false;
+    }
+
+    if (pos_default_add >= 180 && now.Hour() >= 17) {       
+        pos_default_add = 35;
+        servoverti.write(pos_default);
+    }
+
+    if (act_2 == true) {
+        seguimientoFotorresistencias();
+    }
+}
+
+void seguimientoFotorresistencias() {
+    int servoh = servohori.read();
+    int servov = servoverti.read();
+
+    int topl = analogRead(ldrtopl);
+    int topr = analogRead(ldrtopr);
+    int botl = analogRead(ldrbotl);
+    int botr = analogRead(ldrbotr);
+
+    int avgtop = (topl + topr) / 2;
+    int avgbot = (botl + botr) / 2;
+    int avgleft = (topl + botl) / 2;
+    int avgright = (topr + botr) / 2;
+
+    if (avgtop > avgbot) {
+        servoverti.write(servov - 1);
+        if (servov > servovLimitHigh) {
+            servov = servovLimitHigh;
+        }
+        delay(5);
+    }
+    else if (avgbot > avgtop) {
+        servoverti.write(servov + 1);
+        if (servov < servovLimitLow) {
+            servov = servovLimitLow;
+        }
+        delay(5);
+    } else {
+      servoverti.write(servov);
+    }
+
+    if (avgleft < avgright) {
+        servohori.write(servoh - 1);
+        if (servoh > servohLimitHigh) {
+            servoh = servohLimitHigh;
+        }
+        delay(5);
+    }
+    else if (avgright < avgleft) {
+        servohori.write(servoh + 1);
+        if (servoh < servohLimitLow) {
+            servoh = servohLimitLow;
+        }
+        delay(5);
+    } else {
+      servohori.write(servoh);
+    }
+
+    delay(50);
+}
+
+#define countof(a) (sizeof(a) / sizeof(a[0]))
+
+void printDateTime(const RtcDateTime& dt)
+{
+    char datestring[20];
+
+    snprintf_P(datestring, 
+            countof(datestring),
+            PSTR("%04u/%02u/%02u %02u:%02u:%02u %s"),
+            dt.Year(),
+            dt.Month(),
+            dt.Day(),
+            dt.Hour(),
+            dt.Minute(),
+            dt.Second(),
+            DiaSemana[dt.DayOfWeek()]);
+    Serial.println(datestring);
 }
